21fbc116e208211fe90409cb60b7212c5f5b4df5
David Rios <david.rios.gomes@gmail.com>
2026-03-27 15:52:20 -0300

    Export event types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { register } from './infinite-scroller'
 
 export { InfiniteScroller, register } from './infinite-scroller'
-export type { PageResult, FetchPageFn, RenderItemFn } from './infinite-scroller'
+export type {
+  PageResult,
+  FetchPageFn,
+  RenderItemFn,
+  PageChangedEvent,
+  PagesFetchedEvent,
+  ItemElementRemovedEvent,
+} from './infinite-scroller'
 
 if (typeof window !== 'undefined') {
   register()
